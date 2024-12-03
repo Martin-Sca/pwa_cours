aab51a40e259791f26d127e0daa24187b081feee
Martin Scarnière <martin.scarniere@gmail.com>
2024-12-03 10:43:06 +0100

    add src

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ fetch("data.json")
         console.log(response)
         $articles.innerHTML = ""
         response.forEach(data => {
-        const template = 
-                <article class="article">
-                    <div class="" style="height: 200px;">
-                    <img src="${data.image}" alt="Photo de ${data.title}"></div>
-                    <div class="article-title">${data.title1}</div>
-                    <div cass="article-subtitle">${data.title2}</div>
-                    <div class="article-content">${data.content}</div>
-                    <div class="article-meta">${data.date} - ${data.author}</div>
-                </article>
-        
+        const template = `
+            <article class="article">
+                <div class="" style="height: 200px;">
+                <img src="${data.image}" alt="Photo de ${data.title}"></div>
+                <div class="article-title">${data.title1}</div>
+                <div cass="article-subtitle">${data.title2}</div>
+                <div class="article-content">${data.content}</div>
+                <div class="article-meta">${data.date} - ${data.author}</div>
+            </article>
+        `
     $articles.innerHTML += template
         })
     })
